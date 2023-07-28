8fdafa687bb62593fd7d249bad03d6f82e62a306
cristopher <emailcristopher@gmail.com>
2023-07-28 12:40:23 +0800

    new template

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "Welcome to my portfolio website! <span>I'm Cristopher Apoyon</span> a dedicated computer engineering student with a passion for programming. I enjoy solving real-world programming challenges and collaborating with dynamic teams. My portfolio showcases projects that demonstrate my diverse programming skills and love for creating efficient solutions. Let's connect and discuss potential collaborations. Thank you for visiting!"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
